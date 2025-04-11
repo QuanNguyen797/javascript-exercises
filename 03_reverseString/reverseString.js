15ab0510a8c906b19b1e95fae9bce93435594d77
Quan Nguyen <hoangquan797@gmail.com>
2025-04-11 16:06:56 +0700

    Exercise 3: Created reverseString function to reverse input strings.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let drow= word.slice(-1);
+    for(i=0; i< word.length; i++){
+        drow += word.slice(-(i+2),-(i+1))
+    };
+    return drow;
 };
 
 // Do not edit below this line
