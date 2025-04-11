89e34570a07775aee837afe9a2746c9f23b2c40d
Quan Nguyen <hoangquan797@gmail.com>
2025-04-11 15:00:24 +0700

    Complete exercises 1 and 2: helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+const repeatString = function(word, num) {
+    let phrase = "";
+    if(num<0){return 'ERROR'}
+    else {for(i=0; i<num; i++){
+     phrase += word;
+      
+    }
+    return phrase;  }
 };
 
 // Do not edit below this line
