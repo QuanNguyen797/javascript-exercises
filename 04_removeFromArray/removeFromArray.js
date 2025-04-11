3a8930f7f057153d0bec2c8e7a273e38ed93361a
Quan Nguyen <hoangquan797@gmail.com>
2025-04-11 23:01:57 +0700

    Complete removeFromArray function with support for multiple values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (items, item, ...manyMoreArgs) {
+
+    while (items.indexOf(item) !== -1) {
+        items.splice(items.indexOf(item), 1);
+    };
+    for (const i of manyMoreArgs) {
+        if (items.indexOf(i) !== -1) {
+            items.splice(items.indexOf(i), 1);
+        }
+        else { continue }
+
+    }
+    return items;
 };
 
 // Do not edit below this line
