65f16d43a3936cce17cd032d2e4d95c001bca65d
Quan Nguyen <hoangquan797@gmail.com>
2025-04-12 19:20:55 +0700

    Complete tempConversion exercise with Fahrenheit to Celsius and vice versa - Included logic to round results to 1 decimal place - Ensured both functions handle negative temperatures correctly

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let result;
+  result= Math.round(((tempF -32)/(9/5))*10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let result;
+  result= Math.round((tempC*(9/5)+32)*10) / 10;
+  return result;
 };
 
 // Do not edit below this line
