a760d825273a7375be2dacba0f52917b24c2c46e
Quan Nguyen <hoangquan797@gmail.com>
2025-04-12 18:41:10 +0700

    - Implemented sumAll function to return the sum of all integers between two given numbers - Added input validation to handle negative numbers, non-integers, and non-number types - Adjusted loop logic to work regardless of number order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (number1, number2) {
+    let result = 0;
+    if (number1 < 0 || number2 < 0) { result = 'ERROR' }
+    else if (number1 % Math.floor(number1) !== 0 || number2 % Math.floor(number2) !== 0) { result = 'ERROR' }
+    else if (typeof number1 !== 'number' || typeof number2 !== 'number') { result = 'ERROR' }
+    else {
+        for (let i = 0; i < (Math.abs(number2 - number1) + 1); i++) {
+            if (number1 <= number2) {
+                result += number1 + i;
+            }
+            else {
+                result += number2 + i;
+            }
+        };
+    }
+    return result;
 };
 
 // Do not edit below this line
