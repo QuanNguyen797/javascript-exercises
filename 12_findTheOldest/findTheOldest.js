7502aa6d353d4b6e16604b8d2a559140e0bcd662
Quan Nguyen <hoangquan797@gmail.com>
2025-05-06 23:09:05 +0700

    Implement custom solution for finding the oldest person
    Used manual map and loop approach to calculate lifespans and identify the oldest person.
    Handled missing yearOfDeath using current year fallback.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const arr1 = arr.map(
+        (person) => {
+            let now=new Date().getFullYear();
+            if (person.yearOfDeath) {return person.yearOfDeath - person.yearOfBirth}
+                    else {return now-person.yearOfBirth}}
+        );
 
-};
+    let max = 0;
+    for (i = 0; i < arr1.length; i++) {
+        if (max < arr1[i]) {
+            max = arr1[i];
+        };
+    }
 
+    for (i = 0; i < arr1.length; i++) {
+        if (max == arr1[i]) {
+            return arr[i];
+        }
+    }
+}
 // Do not edit below this line
 module.exports = findTheOldest;
